f75b3b41411b9adc3c850311461579133bb3956a
Franciel Ferreira <72324148+FrancielFerreira@users.noreply.github.com>
2022-12-23 03:04:36 -0300

    listas

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,40 +1,14 @@
 import "./App.css";
-import Condicional from "./components/Condicional";
-// import HelloWorld from "./components/HelloWorld";
-// import SayMyName from "./components/SayMyName";
-// import Pessoa from "./components/Pessoa";
-// import Frase from "./components/Frase";
-// import List from "./components/List";
-// import Events from "./components/Events";
-// import Form from "./components/Form";
+import OutraLista from "./components/OutraLista";
 
 function App() {
-  // const imgURL = "https://via.placeholder.com/800x160";
-  // const pp = {
-  //   nome: "João",
-  //   idade: 22,
-  //   profissao: "dev",
-  //   foto: "https://via.placeholder.com/200x160",
-  // };
+  const meusItens = ["React", "Vue", "Angular"];
 
   return (
     <div className="App">
       <h1>Olá mundo!</h1>
-      <Condicional />
-      {/* <Events numero="1" />
-      <Form />
-      <List />
-      <Frase />
-      <SayMyName nome="Franciel" />
-      <SayMyName nome="José" />
-      <Pessoa
-        nome={pp.nome}
-        idade={pp.idade}
-        profissao={pp.profissao}
-        foto={pp.foto}
-      />
-      <img src={imgURL} alt="minha foto" />
-      <HelloWorld /> */}
+      <OutraLista itens={meusItens}/>
+      <OutraLista itens={[]}/>
     </div>
   );
 }
